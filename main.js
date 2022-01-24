6f1c418abfe2af53d49378a8132d01791388ec48
Joseph Grigoletti <jgrig2@gmail.com>
2022-01-23 18:54:59 -0600

    yaml

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA86ylVfPKqbkXiHuTflsLhZfsUUeVMyyU",
+    authDomain: "depaul-test.firebaseapp.com",
+    projectId: "depaul-test",
+    storageBucket: "depaul-test.appspot.com",
+    messagingSenderId: "1044613337520",
+    appId: "1:1044613337520:web:a809f64c06b3fafa368675"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
 ;
 (function() {
 
